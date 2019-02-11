33ba5aea5df43850b441190ede1f26e8c180d23a
MohanrajS19 <mohanraj.sambath@ibotsystems.com>
2019-02-11 16:37:21 +0530

    Kotlin - Literal Constants Added.

--- app/src/main/java/mohan/com/mykotlinexperiments/LiteralConstants.kt
+++ app/src/main/java/mohan/com/mykotlinexperiments/LiteralConstants.kt
@@ -36,7 +36,8 @@ class LiteralConstants{
     val creditCardNumber = 1234_5678_9012_3456L
     val socialSecurityNumber = 999_99_9999L
     val hexBytes = 0xFF_EC_DE_5E
-    val bytes = 0b11010010_01101001_10010100_10010010
+    //val bytes = 0b11010010_01101001_10010100_10010010 //3530134674
+    val bytes = 0b11010010_01101001_10010100_10010010_0000
 
 
     fun showLiteralConstants(){
