13e77aa3c2744d727c40b6a615f9267fd1b94f83
MohanrajS19 <mohanraj.sambath@ibotsystems.com>
2019-11-08 16:20:57 +0530

    Done In KotlinMVVM modules:
    #1 Android MVVM Architecture Tutorial - Introduction
    #2 Android MVVM Architecture Tutorial - Project Setup
    #3 Android MVVM Architecture Tutorial - Using Data Binding
    #4 Android MVVM Architecture Tutorial - User Login using Retrofit
    #5 Android MVVM Architecture Tutorial - Room Database Setup
    
    Refernce Url:
    1- https://www.youtube.com/watch?v=spkIB6GkO1s&list=PLk7v1Z2rk4hjVaZ8DZKe8iT9RIM9OUrwp&index=5

---
+++ kotlinmvvm/src/main/java/com/kotlinmvvm/data/database/UserDao.kt
@@ -0,0 +1,31 @@
+package com.kotlinmvvm.data.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.kotlinmvvm.data.database.entities.CURRENT_USER_ID
+import com.kotlinmvvm.data.database.entities.User
+
+
+/*
+ * Copyright (c) 2019. Created by Mohanraj.S,Innobot Systems on 8/11/19 for KOTLINworkspace
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+interface UserDao{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(user:User):Long
+    @Query("SELECT * FROM user WHERE uid = $CURRENT_USER_ID")
+    fun getUser():LiveData<User>
+
+}
