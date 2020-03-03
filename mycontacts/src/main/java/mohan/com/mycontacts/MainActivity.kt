899b6f2ef4403c425d281192ff0b3b07d3129b16
MohanrajS19 <mohanraj.sambath@ibotsystems.com>
2020-03-03 18:19:25 +0530

    Done: 1- Added Splash Screen 2- Added new  vector background images -------------- Created module for fetch the contact list from device contact. Ref Link: https://medium.com/@manuaravindpta/fetching-contacts-from-device-using-kotlin-6c6d3e76574f Ref GitLink: https://github.com/mohanrajsambath/KotlinFetchingContacts Ref Designlink: https://icons8.com/vector-creator/illustration/5e5e40a501d0360015186fe5 Ref Designlink: https://convertio.co/download/646fe8c12a0f83ec1027b7658bf35f3d1509f9/

--- mycontacts/src/main/java/mohan/com/mycontacts/MainActivity.kt
+++ mycontacts/src/main/java/mohan/com/mycontacts/MainActivity.kt
@@ -18,7 +18,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        loadContacts!!.setOnClickListener { loadContacts() }
+        loadContacts()
+        //loadContacts!!.setOnClickListener { loadContacts() }
     }
 
     private fun loadContacts() {
---
+++ mycontacts/src/main/java/mohan/com/mycontacts/SplashActivity.kt
@@ -0,0 +1,40 @@
+package mohan.com.mycontacts
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+/*
+ * Copyright (c) 2020. Created by Mohanraj.S,Innobot Systems on 3/3/20 for KOTLINworkspace
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class SplashActivity  : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long=3000 // 3 sec
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this,MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
